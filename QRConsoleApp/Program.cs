57c8a359b4f232ec23db4bd263bb56fbd0ac8bae
ThinkBright1412914 <nabinthekishor@gmail.com>
2024-11-10 17:16:40 +0545

    Add project files.

---
+++ QRConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using QRCoder;
+
+Console.WriteLine("Welcome to QR Code Generator");
+Console.Write("Input the URL to convert : ");
+string url = Console.ReadLine();
+
+if (string.IsNullOrEmpty(url))
+{
+	Console.WriteLine("Invalid Input");
+	return;
+}
+
+var qrGenerator = new QRCodeGenerator();
+var qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+
+var qrCode = new BitmapByteQRCode(qrCodeData);
+var qrCodeImage = qrCode.GetGraphic(pixelsPerModule: 20);
+
+var currentDate = DateTime.Now.ToString("dddd, dd MMMM yyyy");
+
+string downloadsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+string filePath = Path.Combine(downloadsPath, $"QRCode_{currentDate}.png");
+
+using (var stream = new FileStream(filePath, FileMode.Create))
+{
+	stream.Write(qrCodeImage, 0, qrCodeImage.Length);
+}
+
+Console.WriteLine($"QR Code saved to {filePath}");
+
